5b43c1720bf4b13a2c9cf2fc1273467453299f1b
vipinjain1004 <vipinjain1004@gmail.com>
2023-03-14 04:10:37 -0700

    main class name change

---
+++ src/main/java/com/jain/schl/svcstdntdtl/StudentDetailsApplication.java
@@ -0,0 +1,15 @@
+package com.jain.schl.svcstdntdtl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentDetailsApplication {
+
+	
+	public static void main(String[] args) {
+
+		SpringApplication.run(StudentDetailsApplication.class, args);
+	}
+	
+}
